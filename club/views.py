33979a8ef091a491fa07820020dd8f6efe3a5fc2
matejmeglic <matej@matejmeglic.com>
2021-01-15 01:26:53 +0100

    Material UI + JSON BE

--- club/views.py
+++ club/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book, Reader
 import os
+from datetime import date
 from django.http.response import JsonResponse, HttpResponse
 from django.conf import settings
 from django.http.response import JsonResponse
@@ -9,26 +10,65 @@ from django.views.generic.base import TemplateView
 
 
 def index(request):
-    books = Book.objects.order_by("-date_read")
+    # get data from db
+    query_books = Book.objects.order_by("date_read")
+    query_readers = Reader.objects.distinct()
 
-    data = []
-    reader = []
+    books = []
+    readers_dict = []
+    books_per_reader = []
+    readers_cumulative = []
 
     # Transform data for graphs
-    for element in books:
-        # all data presented
-        data.append(
+    for reader in query_readers:
+        readers_dict.append({"reader": str(reader.name)})
+
+    for book in query_books:
+        books.append(
             {
-                "reader": str(element.reader),
-                "date_read": element.date_read,
-                "author": element.author,
-                "name": element.name,
-                "description": element.description,
+                "date_read": book.date_read,
+                "reader": str(book.reader),
+                "author": book.author,
+                "name": book.name,
+                "description": book.description,
             }
         )
 
-        reader.append(str(element.reader))
+    for reader in readers_dict:
+        readers_collection = []
+        count = 0
+        previous_date = date(2000, 1, 1)
+        for book in books:
+            if book.get("reader") == reader.get("reader"):
+                if count == 0:
+                    days_from_last_book = 0
+                    previous_date = book.get("date_read")
+                else:
+                    days_from_last_book = (book.get("date_read") - previous_date).days
+                count += 1
 
-    context = {"data": data, "reader": reader}
+                previous_date = book.get("date_read")
+                readers_collection.append(
+                    {
+                        "date_read": book.get("date_read"),
+                        "reader": book.get("reader"),
+                        "author": book.get("author"),
+                        "name": book.get("name"),
+                        "description": book.get("description"),
+                        "days_from_last_book": days_from_last_book,
+                    }
+                )
+        books_per_reader.append(
+            {"reader": reader.get("reader"), "books_read": readers_collection}
+        )
+        readers_cumulative.append({"reader": reader.get("reader"), "books_read": count})
+
+    all_data = {
+        "books": books,
+        "books_per_reader": books_per_reader,
+        "readers_cumulative": readers_cumulative,
+        "readers_dict": readers_dict,
+    }
+    context = {"all_data": all_data}
 
     return render(request, "index.html", {"context": context})
--- src/App.js
+++ src/App.js
@@ -1,55 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   const json = JSON.parse(document.getElementById("json").textContent);
 
-  const reader = JSON.parse(
-    document.getElementById("reader").textContent
-  ).sort();
+  // const reader = JSON.parse(
+  //   document.getElementById("reader").textContent
+  // ).sort();
 
-  var a = [],
-    b = [],
-    prev;
+  // var a = [],
+  //   b = [],
+  //   prev;
 
-  for (var i = 0; i < reader.length; i++) {
-    if (reader[i] !== prev) {
-      a.push(reader[i]);
-      b.push(1);
-    } else {
-      b[b.length - 1]++;
-    }
-    prev = reader[i];
-  }
+  // for (var i = 0; i < reader.length; i++) {
+  //   if (reader[i] !== prev) {
+  //     a.push(reader[i]);
+  //     b.push(1);
+  //   } else {
+  //     b[b.length - 1]++;
+  //   }
+  //   prev = reader[i];
+  // }
 
-  var reader_result = [];
-  for (var i = 0; i < a.length; i++) {
-    reader_result.push({ label: a[i], value: b[i] });
-  }
-
+  // var reader_result = [];
+  // for (var i = 0; i < a.length; i++) {
+  //   reader_result.push({ label: a[i], value: b[i] });
+  // }
   console.log(json);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Dashboard />
+
+      {/* {json.map((el) => (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {el["reader"]}: {el["author"]}-{el["name"]} {el["description"]}
         </p>
-        {json.map((el) => (
-          <p>
-            {el["reader"]}: {el["author"]}-{el["name"]} {el["description"]}
-          </p>
-        ))}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))} */}
     </div>
   );
 }
